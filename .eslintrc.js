ef904851987a719a4dacad2c8a868c0b9e6d9f1d
Robert Rossmann <rr.rossmann@me.com>
2016-03-19 21:50:54 +0100

    Spread operator is not available on Node.js 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,9 @@ module.exports = {
   ],
 
   rules: {
+    // Node.js 4 does not support spread
+    'prefer-spread': 0,
+
     // If your editor cannot find and show these to you, occasionally turn this off and run the
     // linter
     'no-warning-comments': 0,
